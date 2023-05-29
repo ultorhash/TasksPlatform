a9cebca1e7e61cd84bb54c56273e7f00a0609fdf
ultorhash <ultorhash@gmail.com>
2023-05-29 22:49:10 +0200

    home page

--- tasks-platform/src/app/components/home/home.component.ts
+++ tasks-platform/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HexString } from 'src/app/types';
+import { contractAddress } from 'src/app/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  public readonly iconChWidth: number = 8;
+  public contractAddress: HexString = contractAddress;
 }
--- tasks-platform/src/app/modules/common.module.ts
+++ tasks-platform/src/app/modules/common.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ClipboardModule
   ]
 })
 export class CommonModule {}
